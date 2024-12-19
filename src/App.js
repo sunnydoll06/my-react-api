07d520547502d76b49e588b2c4052609dc1fb16c
MENGYO\hu550 <sallylove60827@gmail.com>
2024-12-19 12:11:10 +0800

    modify router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import Login from "./pages/Login";
 import Table from './pages/Table';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/table" element={<Table />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
